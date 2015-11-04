5fc92452f3ade52afa5812d05a181cfb2ad38579
Blake Embrey <hello@blakeembrey.com>
2015-11-04 00:40:43 -0800

    v0.0.12

--- src/bin/typings.ts
+++ src/bin/typings.ts
@@ -3,7 +3,7 @@
 import minimist = require('minimist')
 import { spawn } from 'child_process'
 
-const VERSION = '0.0.11'
+const VERSION = '0.0.12'
 
 const ALIASES: { [cmd: string]: string } = {
   // Install.
