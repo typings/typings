0892f2323f4340e0c5e2375aed234af2eaf02ffe
Blake Embrey <hello@blakeembrey.com>
2015-11-11 16:52:33 -0800

    Remove unused module dependencies

--- custom_typings/object-values.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'object-values' {
-  function values <T> (obj: { [key: number]: T}): T[]
-
-  export = values
-}
--- custom_typings/thenify-all.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'thenify-all' {
-  function thenifyAll (source: any, dest?: any, methods?: string[]): any
-
-  export = thenifyAll
-}
