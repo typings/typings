4d4770d3da17ec350c4ee15957b911bf493e9f7e
Blake Embrey <hello@blakeembrey.com>
2016-01-18 16:47:30 -0800

    Proxy SSL options to HTTP(s) requests

--- src/utils/rc.ts
+++ src/utils/rc.ts
@@ -3,6 +3,10 @@ import { PROJECT_NAME } from './config'
 
 export interface RcConfig {
   proxy?: string
+  rejectUnauthorized?: boolean
+  ca?: string | string[]
+  key?: string
+  cert?: string
 }
 
 export default rc(PROJECT_NAME) as RcConfig
