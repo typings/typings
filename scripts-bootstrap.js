984c35ac50ce9fb828aedfaf8e597a848f2e1af4
John Reilly <johnny_reilly@hotmail.com>
2016-03-06 19:14:06 +0000

    Changed approach

--- scripts-bootstrap.js
+++ scripts-bootstrap.js
@@ -1,4 +1,8 @@
-require('shelljs/global');
+require('shelljs/global')
+
+mkdir('-p', 'typings')
+
+touch('typings/main.d.ts')
 
 echo('SKIP_PREPUBLISH=true')
 env['SKIP_PREPUBLISH'] = true
@@ -8,13 +12,13 @@ exec('rimraf dist')
 
 if (!which('tsc')) {
   echo('Sorry, this script requires tsc');
-  exit(1);
+  exit(1)
 }
 
 if (exec('tsc', {silent:true}).code !== 0) {
-  echo('tsc completed intial build as expected');
+  echo('tsc completed intial build as expected')
 }
 else {
-  echo('tsc completed without errors... that is unexpected and may merit investigation...');
-  exit(1);
+  echo('tsc completed without errors... that is unexpected and may merit investigation...')
+  exit(1)
 }
