4fe2c50afd7ad0619028defdba37ccf10caf5d0a
Homa Wong <homawong@gmail.com>
2016-04-03 17:49:58 -0700

    Default name to (no name)

--- src/support/cli.ts
+++ src/support/cli.ts
@@ -113,7 +113,7 @@ export interface ArchifyOptions {
 /**
  * Make the dependency into the CLI name.
  */
-function toDependencyName (name: string, node: DependencyTree, suffix?: string) {
+function toDependencyName (name: string = chalk.gray('(no name)'), node: DependencyTree, suffix?: string) {
   const fullname = node.version ? `${name}@${node.version}` : name
 
   return suffix ? `${fullname} ${suffix}` : fullname
